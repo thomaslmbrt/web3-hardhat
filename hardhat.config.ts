49fbe1e3b1c90708aaaa19ae333a04720b55c331
Thomas Lambert <tlambert@ippon.fr>
2022-11-06 21:20:14 +0100

    hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,31 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || '';
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || '';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+      chainId: 5
+    },
+    localhost: {
+      url: "http:///localhost:8545",
+      chainId: 31337,
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true
+  }
 };
 
 export default config;
